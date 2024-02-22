b4819e3753b1f534450ccd6faf4f3c16e8898860
JRKreutzer <jose.kreutzer@gmail.com>
2024-02-21 22:31:10 -0300

    FAQs (Sem responsividade)

---
+++ src/components/FAQs/index.jsx
@@ -0,0 +1,27 @@
+import{FaQuestion} from 'react-icons/fa'
+import { faqs } from '../../data'
+import SectionHead from '../Programs/SectionHead'
+import FAQ from './FAQ'
+
+const FAQs = () => {
+  return (
+    <section className="faqs">
+      <div className="container faqs__container">
+        <SectionHead icon={<FaQuestion />} title="FAQs" />
+        <div className="faqs__wrapper">
+          {
+            faqs.map(({id, question, answer}) => {
+              return <FAQ 
+                        key={id}  
+                        question={question} 
+                        answer={answer}
+                      />
+            })
+          }
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default FAQs
